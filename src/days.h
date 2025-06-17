db58f3518112bb43c345a12938a1b63ebca65219
Erin <erinarsenico@outlook.com>
2025-06-17 20:10:19 -0300

    First commit :3

---
+++ src/days.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef days_h
+#define days_h
+
+void day_one();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "days.h"
+
+int main() {
+	printf("//DAY ONE//\n");
+	day_one();
+
+	printf("//DAY TWO//\n");
+
+	_exit(0);
+}
